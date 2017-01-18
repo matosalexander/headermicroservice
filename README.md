10c98f659117a02d31f7c5cb234bfcc0e7bfb55b
Alexander Matos <alexander0885@gmail.com>
2017-01-18 18:06:38 +0000

    major commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Request Header Parser Microservice [Free Code Camp](https://www.freecodecamp.com/challenges/request-header-parser-microservice)
+
+### User Story
+> I can get the IP address, language and operating system for my browser.
+
+### Example
+`{"ip":"xxx.xxx.xxx.xxx","language":"en-US","software":"Windows 7"}`
+
+[Demo](https://headerparser-micro.herokuapp.com)
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var userAgent=require('useragent');
+var app = express();
+
+var port=process.env.port || 8080;
+
+var object={};
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    var lang=req.headers['accept-language'].split(",");
+    var ip=req.headers['x-forwarded-for'];
+    var soft=userAgent.parse(req.headers['user-agent']);
+    
+    object.ip=ip;
+    object.language=lang[0];
+    object.software=soft.os.family;
+    
+    res.json(object);
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.listen(port, function () {
+  console.log("Example app listening on port " + port +"!" )
+});
